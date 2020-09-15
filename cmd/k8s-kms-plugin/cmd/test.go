07a859796b9b8555ac94e5330af190e276a0f0ac
Robert Fitzpatrick <marcusaureliusalius@gmail.com>
2020-09-15 11:14:43 +0100

    Breaking change, re-applying the verify cert chain calls

--- cmd/k8s-kms-plugin/cmd/test.go
+++ cmd/k8s-kms-plugin/cmd/test.go
@@ -18,14 +18,20 @@ package cmd
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/base64"
 	"encoding/pem"
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/thalescpl-io/k8s-kms-plugin/apis/istio/v1"
 	"golang.org/x/sync/errgroup"
+	"hash"
+	"math/big"
+	"net"
 	"os"
 	"path/filepath"
 	"time"
@@ -34,44 +40,50 @@ import (
 var loop bool
 var maxLoops int
 var loopTime, timeout time.Duration
-var defaultAAD = []byte("this is clear but can't change...")
-var fakeCSR = &x509.CertificateRequest{
-	Raw:                      nil,
-	RawTBSCertificateRequest: nil,
-	RawSubjectPublicKeyInfo:  nil,
-	RawSubject:               nil,
-	Version:                  0,
-	Signature:                nil,
-	SignatureAlgorithm:       0,
-	PublicKeyAlgorithm:       0,
-	PublicKey:                nil,
-	Subject:                  pkix.Name{
-		Country:            nil,
-		Organization:       nil,
-		OrganizationalUnit: nil,
-		Locality:           nil,
-		Province:           nil,
-		StreetAddress:      nil,
-		PostalCode:         nil,
-		SerialNumber:       "",
-		CommonName:         "",
-		Names:              nil,
-		ExtraNames:         nil,
-	},
-	Attributes:               nil,
-	Extensions:               nil,
-	ExtraExtensions:          nil,
-	DNSNames:                 nil,
-	EmailAddresses:           nil,
-	IPAddresses:              nil,
-	URIs:                     nil,
-}
-var fakeCSRBytes []byte
 
-const dummyCaCert = "-----BEGIN CERTIFICATE-----\nMIIGADCCA7SgAwIBAgIQcrIs4GGqbY2CPUOcx6lOLzBBBgkqhkiG9w0BAQowNKAP\nMA0GCWCGSAFlAwQCAQUAoRwwGgYJKoZIhvcNAQEIMA0GCWCGSAFlAwQCAQUAogMC\nASAwLTEQMA4GA1UEChMHQWNtZSBDbzEZMBcGA1UEAxMQdGVzdC5leGFtcGxlLmNv\nbTAeFw0yMDA5MTExNDQwMDRaFw0yMDA5MTIxNDQwMDRaMC0xEDAOBgNVBAoTB0Fj\nbWUgQ28xGTAXBgNVBAMTEHRlc3QuZXhhbXBsZS5jb20wggIiMA0GCSqGSIb3DQEB\nAQUAA4ICDwAwggIKAoICAQDGq6BlA2fFS/46wPJLgoQUXNfUZjLOTnuh35XX7Bli\nbUozSoqOSUZkfoojMAbrxMYsLKWHfqVhUhTmB9rf7dzkUvuzlGGL1njwsueOVMXY\npaBKUkWz0JuGjEbXiitUQ8W7PbJaZm0UHp65Fk/Gp/xmMNKEAyxwP2iXx+bRT14d\nunvYB8yHhmm6GWB0hJOj/Z/8OZenk6LYChIGR7xnsGL0keksVmCjhOLtGBW05gNQ\nB96BKszzpYhkl5UOn1dNh8YTUv7i45b6gG0NCG+GWKiROSJqD6ZrU93znE2x8eVp\nzhBnYkNavCJadmPNvZBYSd+ZB7APOMEvjYWiUpp1LzUKB+wr8k1yQLOE1rKgCNbF\nLQCY055CbBxcGZeokZVGxUFAnfqs/f/Du8rFB6AFKWlYUGfH2IJx1VztNFFvdB4F\n/dyDfJL3oMYXGealgDliuSMPsgv+z20ydGP8p8hzNxcmuxfQn1FaLau8mcJrA+FT\nn9G0HjXoXYMqKXu+470AIu3GRwMlrCcMlmC73ax8yN+3hSMjuXCWykxDg4cx+Hfg\nv60YuXTVNdp4bcgzl3hvPI/RVJw7Fn0scveCVmM9UlsWbhfrPGwwkoaUX8dTjGpo\n+BXUkjq0fX0qGCt1cWa7DphQjeRknmyBJUo/pwf+3wPRNapb6FwaBdW+55Z7S10F\n2QIDAQABo4GzMIGwMA4GA1UdDwEB/wQEAwICBDAdBgNVHSUEFjAUBggrBgEFBQcD\nAQYIKwYBBQUHAwIwDwYDVR0TAQH/BAUwAwEB/zANBgNVHQ4EBgQEAQIDBDBfBggr\nBgEFBQcBAQRTMFEwIwYIKwYBBQUHMAGGF2h0dHA6Ly9vY3NwLmV4YW1wbGUuY29t\nMCoGCCsGAQUFBzAChh5odHRwOi8vY3J0LmV4YW1wbGUuY29tL2NhMS5jcnQwQQYJ\nKoZIhvcNAQEKMDSgDzANBglghkgBZQMEAgEFAKEcMBoGCSqGSIb3DQEBCDANBglg\nhkgBZQMEAgEFAKIDAgEgA4ICAQAbN0mSl7rA5p42mdDGQuXeH+Teqn++TGcnveID\noq/+KQnsTzN+8G4G85/DAWJ5m+U3XutV5AZu7nvGa4okIs7WpAwVIx2ktlNigTFt\n3LzptCYvh/TBIL2UEeuTv9y0HCaSoUtaOwguJtizYUP+j1R40tu2ySbtfY7ChyZX\nouvEd69lNlyevsX8N+1/FiQFhoKn6D9pC7TIzwoBoX1DNMt14AsI63p9t2/NCgKY\njxCfphZklizXzVa3ncGAm17d+5jx44BrZMJ/bJqdgws6O8UAR4sLQ3j3cPYYgql0\nJlk9Ty0wo+wbcR5z3hRKJvLpGqyP4pRM7mXOz4SYxAhMwuCYqNhNTYX8xtI/j+bk\nlkrlRIlo1BXsJrVKVKOj3k+Gt+7YpSnXWV7Qj4sXXDo+cKEqE+WWIz1gyFbg8xnR\nWZOEKOZxYXstS0tGP7zqSV+KtBoDW1s5/pYuakM3OIqwoGO0XnAJh7an1KDF/soN\nhgA9iZkxTg+pAzMcK8JlEHF5o/1nz/Vn+j7S+0RZ8KZbOcYpOa8ydhQeajCsbnyi\ny0vGzE5H0KsWyAZgo9Rf9cdsbK5W+YePdgO0Th3dRnnwu+Z8JF/EagI59pjacUb1\nhRb1Ir36L5cylVf+pLSgVUE6Scxj5rcgvNcvDr1KnapCHyka0aBRrknCNOXFrnDP\n4uSkCQ==\n-----END CERTIFICATE-----\n"
 
-func init() {
-	fakeCSRBytes, _ = x509.CreateCertificateRequest(rand.Reader, fakeCSR, nil)
+const dummyCaCert = "-----BEGIN CERTIFICATE-----\nMIIGADCCA7SgAwIBAgIQAzUe9pVQo20RU9LSiRiDkDBBBgkqhkiG9w0BAQowNKAP\nMA0GCWCGSAFlAwQCAQUAoRwwGgYJKoZIhvcNAQEIMA0GCWCGSAFlAwQCAQUAogMC\nASAwLTEQMA4GA1UEChMHQWNtZSBDbzEZMBcGA1UEAxMQdGVzdC5leGFtcGxlLmNv\nbTAeFw0yMDA5MTUxMDEwNTlaFw0zMDA5MTMxMDEwNTlaMC0xEDAOBgNVBAoTB0Fj\nbWUgQ28xGTAXBgNVBAMTEHRlc3QuZXhhbXBsZS5jb20wggIiMA0GCSqGSIb3DQEB\nAQUAA4ICDwAwggIKAoICAQCuaGKyDvJ0ebW/9Kq7fltuLZhWQJb613EcHc2eV7ht\nejffCYklRJeKONhkozroxsb5y0ETvlWRiBDVBj0Zq0dyHY781N/QJZcBons0cRXV\nYNBd4nUaJ//FufzI1mbSXohpSaV1hkoQ2uTqB4B7yUWaiM1nIx1snzdXJSGhVYxy\nRhdTHMNd/z8ut+dwRojFIiU7S5NXaCc9LL9LryXy1N+VZo6sHK6NZQu27ryE7wv1\nh+bvG6TsfIsmfcv94ghX94olxY/+h38sDrX3LboKt3j8Tktg3amnwuKENYnvTOMZ\nkHkofj6k8kx+lCzJLDi2hCcq3r3ZPoT146mU5v3nwGF0zPSN3+GertuI9rmSvUy+\ngeD5QeWczUgADaALMVBTQY+wEBNhzyWa6O/l/yPErW2epFSibHIyz+97Nlen9CaF\nKBAUhRYVJIaUOCPmCK5VW4ghadF8zflUsgo5s/himfs9CWF12yAEYS1MjhyTTmAa\n0/DymJ0M8kaTuVUoQW6rrPGAzVRQEBeeVa3OJY6mPvOq0XosYGXtROSq9DMPGwcy\no9OlXhw6uD/rBPxNC8cqDZviM3QHKoN4lGatgfuSrowIU5Bi1yzgMxKdouY78OEI\nThtQTw2XxdoUy+Vr0XlQg9gAJqP0mq1O8fu7zjhua9k8Pdm6B0fxGsBa0Yz4MMQn\nIwIDAQABo4GzMIGwMA4GA1UdDwEB/wQEAwICBDAdBgNVHSUEFjAUBggrBgEFBQcD\nAQYIKwYBBQUHAwIwDwYDVR0TAQH/BAUwAwEB/zANBgNVHQ4EBgQEAQIDBDBfBggr\nBgEFBQcBAQRTMFEwIwYIKwYBBQUHMAGGF2h0dHA6Ly9vY3NwLmV4YW1wbGUuY29t\nMCoGCCsGAQUFBzAChh5odHRwOi8vY3J0LmV4YW1wbGUuY29tL2NhMS5jcnQwQQYJ\nKoZIhvcNAQEKMDSgDzANBglghkgBZQMEAgEFAKEcMBoGCSqGSIb3DQEBCDANBglg\nhkgBZQMEAgEFAKIDAgEgA4ICAQCfFnc0Cajm48LiDw2NxSsNMCGCT/uju6KJG3O8\naXG6rEorDJs3uWCdyn6PhzyhqEdPGiBfJVJnmY9OfF8wWx3zXVAxstOp1RIrA3yI\nfIZAMoTsAYYKXH9gMda9wcPMFOFKrjbmDJKk8c3WwXth5NIeqqQPGTTh0ovHVc8Q\nHDSZo3lyBEmUDjrF2qu0VGn4m2kuxFl8lPUAu8lUR2+KLj6XStqhDd6gXCa2/quZ\nSROFRccS5bPEwJh7l1QSqhwHjS0oKU0sIGq6+VPq2TKUcf4F1zaw4dOKqhBbm+o1\nN7K49deaQH3Zb40jR7f2Rw+s86MM1ujS8tu98yRcu8+KPq1vb1fOQlG/UnOAtYd7\n8kej0ot/QYb7NxDqxNqW2vePbkUoOHV9TtRNQDV0hQooWB/GzZGWUrILDRugDwH+\nX7XNzC5ov1TbRpXkvmpBkY80oBFb9P4bCtUb2dmcdxM7KM5dnoHOQ8Fb7aSWcstE\nSOI2qbSnl2/uigjWLayWpn6k1OTszsLQTxAcezNLL6cTI+eWb3oC0KoAP458FtNH\nb/W8F2WNIxCjD9ydVU2JFPRSy1FfAQFhNMPwyIoT4AZ46G/u4gNu/AIPERfCUqdG\nQWUMsGgTs6NVDmo5YeasplU5uYyEvqPnUhZFsxNSPu/wmDiIcjrtIeEym7Dq4MiG\neOMvxQ==\n-----END CERTIFICATE-----"
+const dummyCaPrivKey = "-----BEGIN PRIVATE KEY-----\nMIIJKAIBAAKCAgEArmhisg7ydHm1v/Squ35bbi2YVkCW+tdxHB3Nnle4bXo33wmJ\nJUSXijjYZKM66MbG+ctBE75VkYgQ1QY9GatHch2O/NTf0CWXAaJ7NHEV1WDQXeJ1\nGif/xbn8yNZm0l6IaUmldYZKENrk6geAe8lFmojNZyMdbJ83VyUhoVWMckYXUxzD\nXf8/LrfncEaIxSIlO0uTV2gnPSy/S68l8tTflWaOrByujWULtu68hO8L9Yfm7xuk\n7HyLJn3L/eIIV/eKJcWP/od/LA619y26Crd4/E5LYN2pp8LihDWJ70zjGZB5KH4+\npPJMfpQsySw4toQnKt692T6E9eOplOb958BhdMz0jd/hnq7biPa5kr1MvoHg+UHl\nnM1IAA2gCzFQU0GPsBATYc8lmujv5f8jxK1tnqRUomxyMs/vezZXp/QmhSgQFIUW\nFSSGlDgj5giuVVuIIWnRfM35VLIKObP4Ypn7PQlhddsgBGEtTI4ck05gGtPw8pid\nDPJGk7lVKEFuq6zxgM1UUBAXnlWtziWOpj7zqtF6LGBl7UTkqvQzDxsHMqPTpV4c\nOrg/6wT8TQvHKg2b4jN0ByqDeJRmrYH7kq6MCFOQYtcs4DMSnaLmO/DhCE4bUE8N\nl8XaFMvla9F5UIPYACaj9JqtTvH7u844bmvZPD3ZugdH8RrAWtGM+DDEJyMCAwEA\nAQKCAgBG1grP+xYqjIxvLHZztHx6IXawAYfQ1dQQ8WHkIAi+Hle29O6I/nT2JORu\n64UvqhyCtDT4SeQDOdpsSx5h4JkiFjNPKT7GEZ5lgZK81/lgMvQuTZ32Q6y0qDet\ncrdMVizdZpYXR7WpZt521xkuLa9hdpLGgxKeXYRilqg0GMT0XNd4YERRVyxYU0Vi\n6qL+PkIU9Tsg0yKszBHeMYMeP6uXyJHGAdg7gYDiidBzxYt76/i1wOqZSnRR6+IA\ned+dquKnOLilTm8ue8MYY7AeTiqLf1lKPH19r7/EpuIhGX9bkLxE4BGdePPsrU4L\nXzShnMczuEgvhh8Gp7Lm0XLqv85UsIp1wunVGqivCcBwKMlnV062wcoL3OomT4ka\nhWDJON6BiR2+P2zLZyt3SLypNkkkP42gs9Rnfk/QXIWkKIB5+PrurYd8gOxOgxu4\nJY7Bh4EOkDmA3z+PPdibJR4Jgq8xSEIY5oqE4vq7IdXXlDBWixbWy9QRn1+k444j\nxNHw2aTjJ35xH5hzOL3QyRbeiOC2dyATpErXId3IfkU66Uf88S6okaYN+NmOxXZI\nYk4dNBPRhU/CFo2YhyJf+r8R3zkX1uVunln6rQjQHBsG9tuZrsqXPGhLU1zgJyhg\nVYLgLyJRnVqFaJfAAiHy0tGA7K/lw6PDXaw+KBN0ct3SaPIrkQKCAQEA0QiIyqpq\ntDW4+m2IvoTuoezhzz2GnppYp/RCqDD8DGcE/kQx2aPHQTtnk2FmCWA9XVCMCmdG\ndCQJ2XZi8geSHhPgydSGAlpKbdXJ+f8D4QI6j+tc1lqINQoxAKzqUdeoZ4G+SPE/\nAj16v2W7wTAYIAhwuFC8D29PdgYbPjg5olRhRuk3ZL0LBTGKy6SztYN89WdZd0IX\nxG8xG+8iOMe6vFPUAO4h21p4mBwcTOT9nAGRmV1H4EauWQ9GvGXOOOOxIeE0SQDf\nwWUqyqYMPF8Y2WQjMtGjJXrDnfmu7O1uYXHQ/h0AOUwVr4ILHhNwMvHPJ5RlwpUB\nQdW5ZIvlCGO4DwKCAQEA1Zg1qFRh7VHflL2B/cPn80jDjwo1UJIKLCT7skXhzMXG\n1FdDOKqFMgkazOWluYPmzQJG4UDE0tQD5IGz+Wa673hx5RdCxCTecdwhKV/d6V6L\nzJF/xSSrnwdLmkwKdi5aQlHisKMv9Rb2QKdTLNvjbpdCZNbBm/KFMGj3jmGmk5bn\nDBFW5QFpsok2flRIXcgg27jamefsE7bLf04+QzkA0+cOFhTEpJSWE86cawhq79BP\nyH2pKqgNwlz4CzS9VvzPmx5xPtwbxHN7dAZf4+DRIzul6pfJWcv7GhqFxBq55PoE\nnQhrNEMnRqZYS55jZaf8Ah3x+35yKX8BT0iKSuXrrQKCAQEAkMlKhqY3to8niz5n\nYx+MctgzKGrDXgQmuF719K3JR4Xr7Xqq1Mcecs6E1Y63MHNazdHGzkXuhO+ZaukX\na/FWgkLehq4QDH0h5KYaenDq7OWwTpOGAGtAwQxIGOGsg/fOi7NQbBG9xP10kjIT\nNKLtcvKlsOUq/b3p2iQsppInSYsMviM33S0b/wLr0lZIq8dhvFFTpMlA0Sz7ZQ/k\nMlQVwfCGfgZzqQ4nTaTa7WAHUhG8GfCEopISnVl5c3FIwBrmTENDBfX3BmvekfMl\nsoNkIN+9iauvR3ybFkclpLJorFI7omfQCd/rfV+j97cbFg5roEynl3nCHym8eipz\n/7WifQKCAQB/p3hqIgRk0YnOW3RVNcBqphI6at9yR9XMjE3hPeK1f35VadHDDCaO\nwOJDkvx441wNKk8yUINRfWTWLK5jYAJZHKL1R/GfSGmpouYu1BzMXLUwjcTPDhuD\n79g/XzLhbtKC0G2rI9yFnjOOcHJFXSWP8ta7bZ5IlakERbeuYK4thwKPM827EB0b\nluX6mmSlp/X7W39KfFGbdqQocZrEkkzsWCsTB1Z/Bk6rh8/0KBPBP75vFKsF02pl\nvyp/iAWg93ccPhVwfBwcTOh7b1Pf3X0gkYWXrx+ni0GHWFKZ4V84ejRHpcBse7X9\ng21BxGejWcJDgaIdrHSOWFlwCOqd2MwBAoIBAFyCjmzg6g9728ATW1Zar85k2REb\nE4Sjjpf5cQ8BGA0t9X8VK9dTo7uUD0pYeGqEI00TgXUdo50IiAsdRFR4K8xeQ9eP\nnmjyY7aBR7UCm6ydOsdpdYWICjyfLPdEAjiJVr/zgByZtNn+D3ctCRHeWOYNNKjv\nzgVCYF2NEVn5Fx2nc5hfijVC2/8jjmmTc3ry6Z5BfJtFzjLAt4M+EUQGXA6ifi3v\nUGOJ4OOqlyQVLLic+Y8gJCwrUcUEeS8HuWplTS0jV8Vd3a1sLRuuzGdg2VrEXT4w\ng1HlE7rumKdlFWddzacGCAGXLl3XpX4I9DlDlSqgTGLtiiOZk90ZyKJkEWk=\n-----END PRIVATE KEY-----\n"
+
+func hashCsrTemplate(hashF hash.Hash, csrTemplate *x509.CertificateRequest) (digest []byte, err error) {
+
+	hashF.Reset()
+
+	_, err = hashF.Write([]byte(csrTemplate.Subject.String()))
+	if nil != err {
+		return
+	}
+
+	if nil != csrTemplate.DNSNames {
+		for i := 0; i < len(csrTemplate.DNSNames); i++ {
+			_, err = hashF.Write([]byte(csrTemplate.DNSNames[i]))
+			if nil != err {
+				return
+			}
+		}
+	}
+
+	if nil != csrTemplate.EmailAddresses {
+		for i := 0; i < len(csrTemplate.EmailAddresses); i++ {
+			_, err = hashF.Write([]byte(csrTemplate.EmailAddresses[i]))
+			if nil != err {
+				return
+			}
+		}
+	}
+
+	if nil != csrTemplate.IPAddresses {
+		for i := 0; i < len(csrTemplate.IPAddresses); i++ {
+			_, err = hashF.Write([]byte(csrTemplate.IPAddresses[i]))
+			if nil != err {
+				return
+			}
+		}
+	}
+
+	digest = hashF.Sum(digest)
+
+	return
 }
 
 // testCmd represents the test command
@@ -206,24 +218,75 @@ func runTest() error {
 	logrus.Infof("Test 4 Returned LoadedSKey in PEM Format: %v", out)
 	skey.Public()
 
+
+
+
+
+
+
+
+	// Generate a dummy istiod intermediate CA CSR from this
+	var csrTemplate = &x509.CertificateRequest{
+		Subject: pkix.Name{
+			CommonName:   "test.example.com",
+			Organization: []string{"istio"},
+		},
+		PublicKeyAlgorithm: x509.RSA,
+		SignatureAlgorithm: x509.SHA512WithRSA,
+		DNSNames:           []string{"test.example.com"},
+		EmailAddresses:     []string{"gopher@golang.org"},
+		IPAddresses:        []net.IP{net.IPv4(127, 0, 0, 1).To4()},
+	}
+
+	var istioIntermediateCaCSR []byte
+	if istioIntermediateCaCSR, err = x509.CreateCertificateRequest(rand.Reader, csrTemplate, skey); nil != err {
+		logrus.Fatal(err)
+		return err
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	/*
 		AuthenticatedEncrypt
 	*/
+
+	var aadHashOfSelectedCsrTemplateFields []byte
+	aadHashOfSelectedCsrTemplateFields, err = hashCsrTemplate(sha256.New(), csrTemplate)
+	if nil != err {
+		logrus.Fatal(err)
+		return err
+	}
+
 	logrus.Info("Test 5 AuthenticatedEncrypt ")
 	var aeResp *istio.AuthenticatedEncryptResponse
 	if aeResp, err = ic.AuthenticatedEncrypt(ictx, &istio.AuthenticatedEncryptRequest{
 		KekKid: genKEKResp.KekKid,
 		EncryptedDekBlob: genDEKResp.EncryptedDekBlob,
-		Plaintext: []byte("Hello World"),
-		Aad: defaultAAD,
+		Plaintext: istioIntermediateCaCSR,
+		Aad: aadHashOfSelectedCsrTemplateFields,
 	}); err != nil {
 		logrus.Fatal(err)
 		return err
 	}
 
-	logrus.Infof("Test 5 Returned AuthenticatedEncrypt: %s", aeResp.Ciphertext)
+
 	/*
-		AuthenticatedEncrypt
+		AuthenticatedDecrypt
 	*/
 	logrus.Info("Test 6 AuthenticatedDecrypt ")
 	var adResp *istio.AuthenticatedDecryptResponse
@@ -231,13 +294,23 @@ func runTest() error {
 		KekKid:       genKEKResp.KekKid,
 		EncryptedDekBlob:  genDEKResp.EncryptedDekBlob,
 		Ciphertext:   aeResp.Ciphertext,
-		Aad:          defaultAAD,
+		Aad:          aadHashOfSelectedCsrTemplateFields,
 	}); err != nil {
 		logrus.Fatal(err)
 		return err
 	}
+	logrus.Infof("Test 6 Returned AuthenticatedDecrypt (b64): %s", base64.URLEncoding.EncodeToString(adResp.Plaintext))
 
-	logrus.Infof("Test 6 Returned AuthenticatedDecrypt: %s", adResp.Plaintext)
+
+
+
+
+
+
+
+
+
+
 
 
 	logrus.Info("Test 7 ImportCACert ")
@@ -255,13 +328,131 @@ func runTest() error {
 	logrus.Infof("Test 7 Returned ImportCACert: %v", icResp.Success)
 
 
-	
+	/*
+	 * ================================
+	 *
+	 * Signing of CSR using upstream CA
+	 *
+	 * ================================
+	 */
 
+	var reloadedCsr *x509.CertificateRequest
+	reloadedCsr, err = x509.ParseCertificateRequest(adResp.Plaintext)
+	if nil != err {
+		logrus.Fatal(err)
+		return err
+	}
+
+	var pemCaCertBlock *pem.Block
+	pemCaCertBlock, _ = pem.Decode([]byte(dummyCaCert))
+	var parsedRootCaCert *x509.Certificate
+	parsedRootCaCert, err = x509.ParseCertificate(pemCaCertBlock.Bytes)
+	if nil != err {
+		logrus.Fatal(err)
+		return err
+	}
+
+
+	var pemKeyBlock *pem.Block
+	pemKeyBlock, _ = pem.Decode([]byte(dummyCaPrivKey))
+	var parsedCaPrivKey *rsa.PrivateKey
+	parsedCaPrivKey, err = x509.ParsePKCS1PrivateKey(pemKeyBlock.Bytes)
+	if nil != err {
+		logrus.Fatal(err)
+		return err
+	}
+
+	// Sanity check
+	if nil != reloadedCsr.CheckSignature() {
+		logrus.Fatal(err)
+		return err
+	}
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		logrus.Fatalf("Failed to generate serial number: %v", err)
+	}
+
+	var childTemplate = &x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: reloadedCsr.Subject.Organization,
+			CommonName: reloadedCsr.Subject.CommonName,
+		},
+
+		SignatureAlgorithm: x509.SHA256WithRSAPSS,
+		PublicKeyAlgorithm: x509.RSA,
+
+		SubjectKeyId: []byte{1, 2, 3, 4},
+
+		IsCA:                  true,
+
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(time.Hour * 1),
+
+		KeyUsage:              x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		BasicConstraintsValid: true,
+
+		OCSPServer:            []string{"http://ocsp.example.com"},
+		IssuingCertificateURL: []string{"http://crt.example.com/ca1.crt"},
+	}
+	signedCert, signCertErr := x509.CreateCertificate(rand.Reader, childTemplate, parsedRootCaCert, reloadedCsr.PublicKey, parsedCaPrivKey)
+	if nil != signCertErr {
+		logrus.Fatal(signCertErr)
+		return signCertErr
+	}
+
+	var loadedSignedCert = &x509.Certificate{}
+	loadedSignedCert, err = x509.ParseCertificate(signedCert)
+	if nil != err {
+		logrus.Fatal(err)
+		return err
+	}
+
+	// Sanity check
+	certPool := x509.NewCertPool()
+	certPool.AddCert(parsedRootCaCert)
+	_, err = loadedSignedCert.Verify(x509.VerifyOptions{Roots: certPool})
+	if nil != err {
+		logrus.Fatal(err)
+		return err
+	}
+
+	/*
+	 * ====================================
+	 *
+	 * End signing of CSR using upstream CA
+	 *
+	 * ====================================
+	 */
 
 
 	/*
-		AuthenticatedDecrypt
+	   VerifyCertChain - take the decrypted CSR, sign it using the dummy (upstream CA), then hand over to verify the chain
 	*/
+	logrus.Info("Test 7 VerifyCertChain")
+	chain := make([][]byte, 0)
+	chain = append(chain, signedCert)
+	chain = append(chain, pemCaCertBlock.Bytes)
+
+	var verifyCertChainReq = &istio.VerifyCertChainRequest{
+		Certificates: chain,
+	}
+	var verifyCertChainResp = &istio.VerifyCertChainResponse{}
+	if verifyCertChainResp, err = ic.VerifyCertChain(ictx, verifyCertChainReq); nil != err {
+		logrus.Fatal(err)
+		return err
+	}
+
+	if !verifyCertChainResp.SuccessfulVerification {
+		logrus.Fatal("VerifyCertChain returned false")
+		return fmt.Errorf("VerifyCertChain returned false")
+	}
+	logrus.Printf("VerifyCertChain succeeded")
+
+
 
 	return nil
 }
