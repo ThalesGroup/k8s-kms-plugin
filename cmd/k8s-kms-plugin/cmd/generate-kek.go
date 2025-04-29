243f06f0bfb0c39b088c2eb2a863546c77bdab68
Nicolas-Peiffer <102670102+Nicolas-Peiffer@users.noreply.github.com>
2025-04-29 19:05:59 +0200

    use local flags instead of root persistentflags
    Signed-off-by: Nicolas-Peiffer <102670102+Nicolas-Peiffer@users.noreply.github.com>

--- cmd/k8s-kms-plugin/cmd/generate-kek.go
+++ cmd/k8s-kms-plugin/cmd/generate-kek.go
@@ -2,15 +2,38 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"time"
 
 	istio "github.com/ThalesGroup/k8s-kms-plugin/apis/istio/v1"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+// ViperFlagsGenerateKEK defines a struct to hold the values of the flags for the generate-kek command
+type ViperFlagsGenerateKEK struct {
+	Socket   string
+	Timeout  time.Duration
+	KekKeyID string
+}
+
+var vprFlgsGenerateKEK ViperFlagsGenerateKEK
 
 var generateKEKCmd = &cobra.Command{
 	Use:     "generate-kek",
 	Short:   "Generate a KEK",
 	GroupID: "kmscmdsgrpsupporting",
+	// there is no need for to call InitViperSubCmdE PersistentPreRunE since there are no local
+	// flags to pass to this command
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if err := InitViperSubCmdE(viper.GetViper(), cmd, &vprFlgsGenerateKEK); err != nil {
+			logrus.WithField("cobra-cmd", cmd.Use).WithError(err).Error("Error initializing Viper")
+			return err
+		}
+		return nil
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if err := generateKEK(); err != nil {
 			return err
@@ -19,15 +42,20 @@ var generateKEKCmd = &cobra.Command{
 	},
 }
 
+// generateKEK sends a GenerateKEKRequest to the KMS and prints the generated KEK ID
+// Inputs:
+//   - KMS socket path
+//   - KMS timeout
+//   - KMS KEK ID
 func generateKEK() error {
-	ctx, cancel, c, err := istio.GetClientSocket(vprFlgsRoot.SocketPath, vprFlgsRoot.Timeout)
+	ctx, cancel, c, err := istio.GetClientSocket(vprFlgsGenerateKEK.Socket, vprFlgsGenerateKEK.Timeout)
 	defer cancel()
 	if err != nil {
 		return fmt.Errorf("could not open socket: %v", err)
 	}
 
 	genKEKResp, err := c.GenerateKEK(ctx, &istio.GenerateKEKRequest{
-		KekKid: []byte(vprFlgsRoot.KekKeyID),
+		KekKid: []byte(vprFlgsGenerateKEK.KekKeyID),
 	})
 	if err != nil {
 		return fmt.Errorf("generate KEK failed: %v", err)
@@ -39,4 +67,11 @@ func generateKEK() error {
 
 func init() {
 	rootCmd.AddCommand(generateKEKCmd)
+
+	// Since this project uses Viper bind with Cobra flags, we generally do not need to use "Flags().*Var"
+	// (like StringVar, BoolVar, Uint16Var, etc...) as we do not need to access the cobra flag values directly. This is
+	// because we use Viper to retrieve the values of the flags.
+	generateKEKCmd.Flags().String("socket", filepath.Join(os.TempDir(), "run", "hsm-plugin-server.sock"), "Unix Socket. Example: /run/user/$(id -u $USER)/k8s-kms-plugin.sock. Env var: K8S_KMS_PLUGIN_GENERATE_KEK_SOCKET")
+	generateKEKCmd.Flags().Duration("timeout", 5*time.Second, "KMS timeout")
+	generateKEKCmd.Flags().String("kek-id", defaultKekId, "Key ID for KMS KEK. Env var: K8S_KMS_PLUGIN_GENERATE_KEK_KEK_ID")
 }
