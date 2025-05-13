500a9d0b5272b475179857c6714d5fa67dab20cd
Nicolas-Peiffer <102670102+Nicolas-Peiffer@users.noreply.github.com>
2025-05-13 10:48:33 +0200

    use local Viper flags and not inherit flags from the root command
    Signed-off-by: Nicolas-Peiffer <102670102+Nicolas-Peiffer@users.noreply.github.com>

--- cmd/k8s-kms-plugin/cmd/import-ca.go
+++ cmd/k8s-kms-plugin/cmd/import-ca.go
@@ -18,33 +18,54 @@ package cmd
 import (
 	"context"
 	"os"
+	"path/filepath"
+	"time"
 
 	istio "github.com/ThalesGroup/k8s-kms-plugin/apis/istio/v1"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+// buffer to hold the CA certificate
 var caCertPem []byte
-var caCertPath string
+
+type ViperFlagsImportCa struct {
+	CaCertPath string        `mapstructure:"cert-file"`
+	CaID       string        `mapstructure:"ca-id"`
+	SocketPath string        `mapstructure:"socket"`
+	Timeout    time.Duration `mapstructure:"timeout"`
+}
+
+// Initialize the ViperFlagsImportCa struct with all the import-ca cobra CLI flags bound to Viper env vars & config file
+var vprFlgsImportCa ViperFlagsImportCa
 
 // importCaCmd represents the import-ca command
 var importCaCmd = &cobra.Command{
 	Use:     "import-ca",
 	Short:   "Import CA certificate",
 	GroupID: "kmscmdsgrpsupporting",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if err := InitViperSubCmdE(viper.GetViper(), cmd, &vprFlgsImportCa); err != nil {
+			logrus.WithField("cobra-cmd", cmd.Use).WithError(err).Error("Error initializing Viper")
+			return err
+		}
+		return nil
+	},
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
 		var ictx context.Context
 		var icancel context.CancelFunc
 		var ic istio.KeyManagementServiceClient
-		if ictx, icancel, ic, err = istio.GetClientSocket(vprFlgsRoot.SocketPath, vprFlgsRoot.Timeout); err != nil {
+		if ictx, icancel, ic, err = istio.GetClientSocket(vprFlgsImportCa.SocketPath, vprFlgsImportCa.Timeout); err != nil {
 			return
 		}
 		defer icancel()
 
-		if caCertPem, err = os.ReadFile(caCertPath); err != nil {
+		if caCertPem, err = os.ReadFile(vprFlgsImportCa.CaCertPath); err != nil {
 			return
 		}
 		req := &istio.ImportCACertRequest{
-			CaId:       []byte(vprFlgsRoot.CaID),
+			CaId:       []byte(vprFlgsImportCa.CaID),
 			CaCertBlob: caCertPem,
 		}
 		if _, err = ic.ImportCACert(ictx, req); err != nil {
@@ -57,6 +78,11 @@ var importCaCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(importCaCmd)
 
-	importCaCmd.Flags().StringVarP(&caCertPath, "cert-file", "f", "", "Certificate File")
+	importCaCmd.Flags().StringP("cert-file", "f", "", "Certificate File")
 	importCaCmd.MarkFlagRequired("cert-file")
+
+	// Socket & Timeout
+	importCaCmd.PersistentFlags().String("socket", filepath.Join(os.TempDir(), "run", "hsm-plugin-server.sock"), "Unix Socket. Example: /run/user/$(id -u $USER)/k8s-kms-plugin.sock. Corresponding environment variable: K8S_KMS_PLUGIN_SERVE_SOCKET")
+	importCaCmd.PersistentFlags().Duration("timeout", 30*time.Second, "Timeout Duration")
+
 }
